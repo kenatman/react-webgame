9b1876de4b9506e01773c9c8c27e2e9723c9aef1
Yunsu <kenatman0723@gmail.com>
2022-04-22 20:52:12 +0900

    반응속도게임 로직 추가

--- 3.response-check/ResponseCheck.jsx
+++ 3.response-check/ResponseCheck.jsx
@@ -7,8 +7,45 @@ class ResponseCheck extends Component {
         result: []
     }
 
-    handleClickScreen = () => {
+    timeoutId;
+    startTime;
+    endTime;
 
+    handleClickScreen = () => {
+        const { state, message, result } = this.state;
+        if(state === 'waiting') {
+            this.setState({
+                state: 'ready',
+                message: '초록색으로 바뀌면 클릭하세요'
+            });
+            this.timeoutId = setTimeout(() => {
+              this.setState({
+                  state: 'now',
+                  message: '지금 클릭'
+              });
+              this.startTime = new Date();
+            }, Math.floor(Math.random()*1000 + 2000)) // 2~3초 후 실행
+        } else if(state === 'ready') {
+            clearTimeout(this.timeoutId);
+            this.setState({
+                state: 'waiting',
+                message: '너무 성급하셨군요. 초록색으로 바뀌면 클릭하세요.'
+            })
+        } else if (state === 'now') {
+            this.endTime = new Date();
+            this.setState((prev) => ({
+                    state: 'waiting',
+                    message: '클릭해서 시작하세요.',
+                    result: [...prev.result, this.endTime - this.startTime]
+                })
+            )
+        }
+    }
+
+    handleReset = () => {
+        this.setState({
+            result: []
+        })
     }
 
     renderAverage = () => {
@@ -17,7 +54,7 @@ class ResponseCheck extends Component {
             ? null
             : <>
                 <div>평균시간 : {result.reduce((a,c) => a + c) / result.length} ms</div>
-                <button>리셋</button>
+                <button onClick={this.handleReset}>리셋</button>
             </>
     }
 
