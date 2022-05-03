d7d049d3e9242682e44bab13cea44e766f0f69d7
Yunsu <kenatman0723@gmail.com>
2022-05-03 17:11:59 +0900

    게임로직추가 : Object.entries는 이중배열을 반환한다.

--- 4.RSP/RSP.jsx
+++ 4.RSP/RSP.jsx
@@ -7,40 +7,28 @@ const rspCoords = {
 };
 
 const scores = {
-    가위: 1,
-    바위: 0,
+    바위: 1,
+    가위: 0,
     보: -1
+}
+
+const computerChoice = (imgCoord) => {
+    return Object.entries(rspCoords).find(function(v) {
+        return v[1] === imgCoord
+    })[0]
 }
 
 class RSP extends Component {
     state = {
         result: '',
-        imgCoord: '0',
+        imgCoord: rspCoords.바위,
         score: 0,
     }
 
     intervalId;
 
     componentDidMount() {
-        this.intervalId = setInterval(() => {
-            const { imgCoord } = this.state; // 만약 옆의 변수를 setInterval 밖으로 뺀다면 클로저문제 발생... 비동기함수 바깥에 있는 변수를 참조하면 클로저 발생!!!
-            if (imgCoord === rspCoords.바위) {
-                this.setState({
-                    imgCoord: rspCoords.가위
-                });
-                } else if (imgCoord === rspCoords.가위) {
-                this.setState({
-                    imgCoord: rspCoords.보
-                });
-            } else if (imgCoord === rspCoords.보) {
-                this.setState({
-                    imgCoord: rspCoords.바위
-                });
-            }
-        }, 1000)
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
+        this.intervalId = setInterval(this.changeHand, 1000)
     }
 
     componentWillUnmount() {
@@ -53,9 +41,9 @@ class RSP extends Component {
             <>
                 <div id="computer" style={{ background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0` }} />
                 <div>
-                    <button id="rock" className="btn" onClick={this.onClickBtn('바위')}>바위</button>
-                    <button id="scissor" className="btn" onClick={this.onClickBtn('가위')}>가위</button>
-                    <button id="paper" className="btn" onClick={this.onClickBtn('보')}>보</button>
+                    <button id="rock" className="btn" onClick={() => this.onClickBtn('바위')}>바위</button>
+                    <button id="scissor" className="btn" onClick={() => this.onClickBtn('가위')}>가위</button>
+                    <button id="paper" className="btn" onClick={() => this.onClickBtn('보')}>보</button>
                 </div>
                 <div>{result}</div>
                 <div>현재 {score}점</div>
@@ -63,8 +51,46 @@ class RSP extends Component {
         )
     }
 
-    onClickBtn = () => {
+    changeHand = () => {
+        const { imgCoord } = this.state; // 만약 옆의 변수를 setInterval 밖으로 뺀다면 클로저문제 발생... 비동기함수 바깥에 있는 변수를 참조하면 클로저 발생!!!
+        if (imgCoord === rspCoords.바위) {
+            this.setState({
+                imgCoord: rspCoords.가위
+            });
+        } else if (imgCoord === rspCoords.가위) {
+            this.setState({
+                imgCoord: rspCoords.보
+            });
+        } else if (imgCoord === rspCoords.보) {
+            this.setState({
+                imgCoord: rspCoords.바위
+            });
+        }
+    }
 
+    onClickBtn = (choice) => {
+        const { imgCoord } = this.state;
+        clearInterval(this.intervalId);
+        const myScore = scores[choice];
+        const cpuScore = scores[computerChoice(imgCoord)];
+        const diff = myScore - cpuScore;
+
+        if (diff === 0) {
+            this.setState({
+                result: '비겼습니다.'
+            });
+        } else if ([1, -2].includes(diff)) {
+            this.setState((prev) => ({
+                result: '이겼습니다.',
+                score: prev.score + 1
+            }))
+        } else {
+            this.setState((prev) => ({
+                result: '졌습니다.',
+                score: prev.score - 1
+            }))
+        }
+        this.intervalId = setInterval(this.changeHand, 1000)
     }
 }
 
