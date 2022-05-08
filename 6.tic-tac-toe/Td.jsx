22f5b9b1e306ef8ea113e060ff159795d1dc2550
Yunsu <kenatman0723@gmail.com>
2022-05-08 16:00:05 +0900

    최적화하기(memo, useCallback) + 렌더링 원인 찾기 스킬

--- 6.tic-tac-toe/Td.jsx
+++ 6.tic-tac-toe/Td.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
-import {CHANGE_TURN, CLICK_CELL} from "./TicTacToe";
+import React, { memo, useCallback, useEffect, useRef } from 'react';
+import { CLICK_CELL } from "./TicTacToe";
 
-const Td = ({ cellData, rowIndex, cellIndex, dispatch }) => {
-    const handleClickCell = () => {
+const Td = memo(({ cellData, rowIndex, cellIndex, dispatch }) => {
+    const handleClickCell = useCallback(() => {
         if (cellData) return;
         dispatch({
             type: CLICK_CELL,
             rowIndex,
             cellIndex
         });
-    }
+    }, [cellData]);
+
+    // 렌더링 최적화 스킬 : useEffect와 useRef를 활용하여 어떤 props 때문에 렌더링 되는지 알아보는 방법
+    const ref = useRef([]);
+    useEffect(() => {
+        console.log(cellData === ref.current[0], rowIndex === ref.current[1], cellIndex === ref.current[2], dispatch === ref.current[3] )
+        ref.current = [cellData, rowIndex, cellIndex, dispatch];
+    }, [cellData, rowIndex, cellIndex, dispatch])
 
     return (
         <td onClick={handleClickCell}>{cellData}</td>
     )
-}
+})
 
 export default Td;
--- 6.tic-tac-toe/Tr.jsx
+++ 6.tic-tac-toe/Tr.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { memo } from 'react';
 import Td from './Td';
 
-const Tr = ({ rowData, rowIndex, dispatch }) => {
+const Tr = memo(({ rowData, rowIndex, dispatch }) => {
     return (
         <tr>
             {Array(rowData.length).fill().map((v, i) => <Td key={`td-${i}`} dispatch={dispatch}
-                                                             cellData={rowData[i]} rowIndex={rowIndex}
-                                                             cellIndex={i}/>)}
+                                                            cellData={rowData[i]} rowIndex={rowIndex}
+                                                            cellIndex={i}/>)}
         </tr>
     )
-}
+})
 
 export default Tr;
